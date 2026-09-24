bd5455f064be685a5356ef46f382571ebfe53889
Ko Bhone <bhonem908@gmail.com>
2026-09-24 09:17:30 +0630

    phase2: Done step 1 for Data Preprocessing

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -1,5 +1,8 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
 
 
 def clean_products(df: pd.DataFrame) -> pd.DataFrame:
@@ -17,3 +20,14 @@ def split_data(df: pd.DataFrame):
     train_df, temp_df = train_test_split(df, test_size=0.2, random_state=42)
     val_df, test_df = train_test_split(temp_df, test_size=0.5, random_state=42)
     return train_df, val_df, test_df
+
+def create_preprocessor():
+    categorical_features = ["brand"]
+    numerical_features = ["price", "stock", "rating"]
+    preprocessor = ColumnTransformer(
+        transformers = [
+            ("categorical", OneHotEncoder(handle_unknown="ignore"), categorical_features),
+            ("numerical", StandardScaler(), numerical_features)
+        ]
+    )
+    return preprocessor
