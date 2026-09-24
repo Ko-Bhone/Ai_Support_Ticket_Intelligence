7073094f76c8cd19480c70fd593e3285ad5f6acb
Ko Bhone <bhonem908@gmail.com>
2026-09-24 11:09:57 +0630

    phase2: add Model Comparison between Logistic Regression & Random Forest

--- src/ml/classification.py
+++ src/ml/classification.py
@@ -1,10 +1,27 @@
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 from src.data.preprocessing import create_preprocessor
 
 def create_classification_pipeline():
-
     preprocessor = create_preprocessor()
     model = LogisticRegression(max_iter=1000, random_state=42)
-    pipeline = Pipeline(steps=[("preprocessor", preprocessor), ("model", model)])
+    pipeline = Pipeline(
+        steps=[
+            ("preprocessor", preprocessor),
+            ("model", model)
+        ]
+    )
+    return pipeline
+
+
+def create_random_forest_pipeline():
+    preprocessor = create_preprocessor()
+    model = RandomForestClassifier(n_estimators=200, random_state=42, n_jobs=-1)
+    pipeline = Pipeline(
+        steps=[
+            ("preprocessor", preprocessor),
+            ("model", model)
+        ]
+    )
     return pipeline
